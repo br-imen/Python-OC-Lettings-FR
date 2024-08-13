41f4d0675f39156693f2c8ab7b3a6eaf4bba272a
Imenbr <imen.benrhouma93@gmail.com>
2024-08-13 19:03:47 +0200

    templates updates and URL namespacing

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from lettings.views import letting, lettings_index
-from profiles.views import profiles_index, profile
-
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', letting, name='letting'),
-    path('profiles/', profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', profile, name='profile'),
     path('admin/', admin.site.urls),
+    path('lettings/', include('lettings.urls', namespace='lettings')),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
 ]
