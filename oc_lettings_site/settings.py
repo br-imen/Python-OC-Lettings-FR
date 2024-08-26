221439752d46c2f6a633541b6e09064f4f2912c3
Imenbr <imen.benrhouma93@gmail.com>
2024-08-26 17:12:07 +0200

    (feat) use whitenoise for serving staticfiles

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -157,3 +158,5 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
