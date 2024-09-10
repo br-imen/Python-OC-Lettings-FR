71a58e60c8c5969ddc391ff663e4225d1064687c
Imenbr <imen.benrhouma93@gmail.com>
2024-09-10 11:39:56 +0200

    (fix) entrypoint in local

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Check if the environment is not production
+if [ "$ENVIRONMENT" != "production" ]; then
+  # Check if the .env file exists and copy it if it does
+  if [ -f /app/.env ]; then
+    cp /app/.env /etc/secrets/.env
+  fi
+fi
+
+
 # If the environment is production, run collectstatic
 if [ "$ENVIRONMENT" = "production" ]; then
   echo "Running migrate..."
